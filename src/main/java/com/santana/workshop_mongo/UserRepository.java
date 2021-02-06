45bcab997dbdab82146810864a0ef28ea7e98dcf
Eder Sant'ana <edersana@gmail.com>
2021-02-06 12:57:21 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/santana/workshop_mongo/UserRepository.java
@@ -0,0 +1,11 @@
+package com.santana.workshop_mongo;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.santana.workshop_mongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}

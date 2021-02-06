81bf31aad030f51af29bd82b1f4863e692e8d9a8
Eder Sant'ana <edersana@gmail.com>
2021-02-06 14:29:44 -0300

    Inserção de usuário com POST

--- src/main/java/com/santana/workshop_mongo/services/UserService.java
+++ src/main/java/com/santana/workshop_mongo/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.santana.workshop_mongo.UserRepository;
 import com.santana.workshop_mongo.domain.User;
+import com.santana.workshop_mongo.dto.UserDTO;
 import com.santana.workshop_mongo.services.exception.ObjectNotFoundException;
 
 @Service
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado "));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 
 }

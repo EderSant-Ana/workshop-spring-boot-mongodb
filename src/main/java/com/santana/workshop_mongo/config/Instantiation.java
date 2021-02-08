406f5977ca52b5ddd0762811704b7a032abd3493
Eder Sant'ana <edersana@gmail.com>
2021-02-08 17:21:59 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/santana/workshop_mongo/config/Instantiation.java
+++ src/main/java/com/santana/workshop_mongo/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import com.santana.workshop_mongo.domain.Post;
 import com.santana.workshop_mongo.domain.User;
 import com.santana.workshop_mongo.dto.AuthorDTO;
+import com.santana.workshop_mongo.dto.CommentDTO;
 import com.santana.workshop_mongo.repositories.PostRepository;
 import com.santana.workshop_mongo.repositories.UserRepository;
 
@@ -40,10 +41,18 @@ public class Instantiation implements CommandLineRunner{
 		Post post1 = new Post(null, Instant.parse("2018-03-21T00:00:00Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, Instant.parse("2018-03-23T00:00:00Z"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
 		
+		CommentDTO c1 = new CommentDTO("Boa viagem mano!", Instant.parse("2018-03-21T00:00:00Z"), new AuthorDTO(alex));
+		CommentDTO c2 = new CommentDTO("Aproveite!", Instant.parse("2018-03-22T00:00:00Z"), new AuthorDTO(bob));
+		CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", Instant.parse("2018-03-23T00:00:00Z"), new AuthorDTO(maria));
+
+		post1.getComments().addAll(Arrays.asList(c1, c2));
+		post2.getComments().addAll(Arrays.asList(c3));
+		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 		maria.getPosts().addAll(Arrays.asList(post1, post2));
 		userRepository.save(maria);
+				
 	}
 
 }
--- src/main/java/com/santana/workshop_mongo/domain/Post.java
+++ src/main/java/com/santana/workshop_mongo/domain/Post.java
@@ -2,12 +2,15 @@ package com.santana.workshop_mongo.domain;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.santana.workshop_mongo.dto.AuthorDTO;
+import com.santana.workshop_mongo.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable{
@@ -20,6 +23,8 @@ public class Post implements Serializable{
 	private String title;
 	private String body;
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 
@@ -71,6 +76,10 @@ public class Post implements Serializable{
 
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
 	}
 
 	@Override
